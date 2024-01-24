e7852bab56fe646f2009e9db9079faaa9850419f
Muskan Yadav <yadavmu@sheridancollege.ca>
2024-01-24 10:21:43 -0500

    Added part time student class

---
+++ src/arraydemo/PartTimeStudent.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraydemo;
+
+/**
+ *
+ * @author Muskan
+ */
+public class PartTimeStudent extends ArrayDemo {
+    private int numOfCourses;
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+
+    public PartTimeStudent(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+}
+    
+
